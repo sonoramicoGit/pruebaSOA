9e4468c956ad640ada91e0987b557a1e0a03d687
reysoac <rromeroprogramacion@gmail.com>
2021-02-24 12:38:20 -0600

    creacion del proyecto pruebaGitSOA

---
+++ src/app/Negocio.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Negocio {
+
+	
+	public   String saludo() {
+		return "En el metodo saludo()";
+	}
+	
+}
---
+++ src/app/Test.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		Negocio neg=new Negocio();
+		System.out.println(neg.saludo());
+	}
+
+}
